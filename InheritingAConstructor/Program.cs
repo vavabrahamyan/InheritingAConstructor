48b9763b32f288c8dbd5fe13cea7375e25562086
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2021-01-26 14:25:40 +0400

    Добавьте файлы проекта.

---
+++ InheritingAConstructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InheritingAConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating a BaseClass object");
+            BaseClass bc = new BaseClass();
+
+            Console.WriteLine("Now creating a SubClass object");
+            SubClass sc = new SubClass();
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+
+    public class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("\tConstructing BaseClass");
+        }
+    }
+
+    public class SubClass : BaseClass
+    {
+        public SubClass()
+        {
+            Console.WriteLine("\tConstructing SubClass");
+        }
+    }
+}
